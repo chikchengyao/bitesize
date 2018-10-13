5434ba94b3f1c081fe93e0cdb6571379d7e4320e
spikerheado1234 <ahangupta.96@gmail.com>
2018-10-13 20:00:58 +0800

    Fix rendering bug

--- app/src/main/java/sg/bitesize/app/MainActivity.java
+++ app/src/main/java/sg/bitesize/app/MainActivity.java
@@ -104,23 +104,8 @@ public class MainActivity extends AppCompatActivity {
         }
     });
 
-    // When you build a Renderable, Sceneform loads its resources in the background while returning
-    // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
-//    ModelRenderable.builder()
-//              .setSource(this, R.raw.andy)
-//              .build()
-//              .thenAccept(renderable -> andyRenderable = renderable)
-//              .exceptionally(
-//                      throwable -> {
-//                          Toast toast =
-//                                  Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
-//                          toast.setGravity(Gravity.CENTER, 0, 0);
-//                          toast.show();
-//                          return null;
-//                      });
-
       ModelRenderable.builder()
-              .setSource(this, Uri.parse("app/src/main/assets/burger.sfb"))
+              .setSource(this, Uri.parse("burger.sfb"))
               .build()
               .thenAccept(renderable -> andyRenderable = renderable)
               .exceptionally(
