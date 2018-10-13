37b6c454f0b0cfa635264f504db2274961c73274
chikchengyao <chikben@gmail.com>
2018-10-13 18:01:49 +0800

    Add to Util a field and empty method for splash screen

--- app/src/main/java/sg/bitesize/app/Util.java
+++ app/src/main/java/sg/bitesize/app/Util.java
@@ -1,4 +1,11 @@
 package sg.bitesize.app;
 
+import android.app.Activity;
+
 class Util {
+    static boolean isFirstStartup = true;
+
+    static void showSplashScreen(Activity activity){
+        
+    }
 }
