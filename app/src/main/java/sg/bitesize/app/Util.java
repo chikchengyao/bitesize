9fc70acbf31dcbbb67e47455cb2ab1afed4ed172
chikchengyao <chikben@gmail.com>
2018-10-13 18:06:43 +0800

    Handle not-first-startup cases -- skip splash screen

--- app/src/main/java/sg/bitesize/app/Util.java
+++ app/src/main/java/sg/bitesize/app/Util.java
@@ -1,11 +1,19 @@
 package sg.bitesize.app;
 
 import android.app.Activity;
+import android.view.View;
 
 class Util {
-    static boolean isFirstStartup = true;
+    private static boolean isFirstStartup = true;
 
     static void showSplashScreen(Activity activity){
-        
+        View splashScreen = activity.findViewById(R.id.splash_screen_layout);
+
+        if (!isFirstStartup) {
+            splashScreen.setVisibility(View.GONE);
+            return;
+        }
+
+
     }
 }
