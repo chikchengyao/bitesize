1f0ffbac91ad2872eb580de8042abf256dcd0d39
spikerheado1234 <ahangupta.96@gmail.com>
2018-10-13 19:34:30 +0800

    Modify MainActivity to render burger

--- app/src/main/java/sg/bitesize/app/MainActivity.java
+++ app/src/main/java/sg/bitesize/app/MainActivity.java
@@ -18,6 +18,7 @@ package sg.bitesize.app;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
@@ -105,18 +106,31 @@ public class MainActivity extends AppCompatActivity {
 
     // When you build a Renderable, Sceneform loads its resources in the background while returning
     // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
-    ModelRenderable.builder()
-        .setSource(this, R.raw.andy)
-        .build()
-        .thenAccept(renderable -> andyRenderable = renderable)
-        .exceptionally(
-            throwable -> {
-              Toast toast =
-                  Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
-              toast.setGravity(Gravity.CENTER, 0, 0);
-              toast.show();
-              return null;
-            });
+//    ModelRenderable.builder()
+//              .setSource(this, R.raw.andy)
+//              .build()
+//              .thenAccept(renderable -> andyRenderable = renderable)
+//              .exceptionally(
+//                      throwable -> {
+//                          Toast toast =
+//                                  Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+//                          toast.setGravity(Gravity.CENTER, 0, 0);
+//                          toast.show();
+//                          return null;
+//                      });
+
+      ModelRenderable.builder()
+              .setSource(this, Uri.parse("app/src/main/assets/burger.sfb"))
+              .build()
+              .thenAccept(renderable -> andyRenderable = renderable)
+              .exceptionally(
+                      throwable -> {
+                          Toast toast =
+                                  Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                          toast.setGravity(Gravity.CENTER, 0, 0);
+                          toast.show();
+                          return null;
+                      });
 
     arFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
         arFragment.onUpdate(frameTime);
