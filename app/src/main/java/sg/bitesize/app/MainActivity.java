9008ccef59718d313be3610fb4ef68282789d729
ning <ningyuan.sg@gmail.com>
2018-10-13 23:44:20 +0800

    Add the target arrow

--- app/src/main/java/sg/bitesize/app/MainActivity.java
+++ app/src/main/java/sg/bitesize/app/MainActivity.java
@@ -37,6 +37,7 @@ import com.google.ar.core.Plane;
 import com.google.ar.core.Trackable;
 import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
@@ -53,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
   private static final double MIN_OPENGL_VERSION = 3.0;
 
   private ArFragment arFragment;
+  private Anchor targetArrowAnchor;
   private boolean isTracking;
   private boolean isHitting;
   private FloatingActionButton addButton;
@@ -77,10 +79,8 @@ public class MainActivity extends AppCompatActivity {
     addButton = findViewById(R.id.add_button);
     addButton.setOnClickListener((View view) -> {
         Log.i("APP: addButton_OnClick", String.format("isTracking: %s; isHitting: %s", isTracking, isHitting));
-
         renderFoodModel();
         openOrderMenu();
-
     });
 
     portion_button_add = (FloatingActionButton)findViewById(R.id.portion_button_add);
@@ -103,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
         arFragment.onUpdate(frameTime);
         updateTracking();
         updateHitTest();
+        updateTargetArrowNode();
     });
   }
 
@@ -130,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
 
   private CompletableFuture<ModelRenderable> buildRenderable(String uri) {
       return ModelRenderable.builder()
-          .setSource(this, Uri.parse("burger.sfb"))
+          .setSource(this, Uri.parse(uri))
           .build()
           .exceptionally(
                   throwable -> {
@@ -144,14 +145,14 @@ public class MainActivity extends AppCompatActivity {
    */
   private void showToast(String message) {
       Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
-      toast.setGravity(Gravity.CENTER, 0, 0);
+      toast.setGravity(Gravity.BOTTOM, 0, 0);
       toast.show();
   }
 
   /**
    * Renders a ModelRenderable model on a HitResult.
    */
-  private void renderModel(HitResult hitResult, ModelRenderable model) {
+  private Anchor renderModel(HitResult hitResult, ModelRenderable model) {
       Anchor anchor = hitResult.createAnchor();
       AnchorNode anchorNode = new AnchorNode(anchor);
       anchorNode.setParent(arFragment.getArSceneView().getScene());
@@ -162,6 +163,26 @@ public class MainActivity extends AppCompatActivity {
       node.setParent(anchorNode);
       node.setRenderable(model);
       node.select();
+
+      return anchor;
+  }
+
+  private void updateTargetArrowNode() {
+      if (isTracking && isHitting) {
+          Frame frame = arFragment.getArSceneView().getArFrame();
+          android.graphics.Point pt = getScreenCenter();
+          List<HitResult> hits;
+          if (frame != null) {
+              hits = frame.hitTest(pt.x, pt.y);
+              for (HitResult hit : hits) {
+                  if (targetArrowAnchor != null) {
+                      targetArrowAnchor.detach();
+                  }
+                  targetArrowAnchor = renderModel(hit, targetArrowRenderable);
+                  break;
+              }
+          }
+      }
   }
 
   /**
@@ -178,6 +199,8 @@ public class MainActivity extends AppCompatActivity {
   /**
    * Updates the value of isHitting. isHitting is something like whether or not the current center
    * of the screen lies on a plane that is tracked.
+   *
+   * Also reinstantiates a AnchorNode for the targetArrowNode
    */
   private boolean updateHitTest() {
       Frame frame = arFragment.getArSceneView().getArFrame();
@@ -189,8 +212,7 @@ public class MainActivity extends AppCompatActivity {
           hits = frame.hitTest(pt.x, pt.y);
           for (HitResult hit : hits) {
               Trackable trackable = hit.getTrackable();
-              if (trackable instanceof Plane &&
-                      ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+              if (trackable instanceof Plane && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
                   isHitting = true;
                   break;
               }
